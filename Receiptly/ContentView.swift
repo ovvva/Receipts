4464f56bbf3bcd40fbb1cbd85f866cfabe0f93da
Volodymyr <ovvva13@gmail.com>
2026-04-27 12:40:07 -0400

    Initial Commit

---
+++ Receiptly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Receiptly
+//
+//  Created by Volodymyr Miasnykov on 2026-04-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Receiptly/ReceiptlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptlyApp.swift
+//  Receiptly
+//
+//  Created by Volodymyr Miasnykov on 2026-04-27.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
